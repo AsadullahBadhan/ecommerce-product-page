a73cf232026799bbdfecf274cd8b311c8ba2e1fb
AsadullahBadhan <asadullah.badhan@gmail.com>
2024-04-08 01:04:25 +0600

    lightbox functionality added

--- app.js
+++ app.js
@@ -4,10 +4,51 @@ const navMenu = document.querySelector('.nav-menu');
 
 hamburger.addEventListener('click', () => {
   navMenu.classList.add('active');
-  document.body.style.backgroundColor = 'rgba(0, 0, 0, 0.75)';
 });
 
 closeBtn.addEventListener('click', () => {
   navMenu.classList.remove('active');
-  document.body.style.backgroundColor = "white";
 });
+
+//lighthouse
+const thumbnails = document.querySelectorAll('.column img');
+const productImage = document.querySelector('.product-img img');
+
+thumbnails.forEach(thumbnail => {
+  thumbnail.addEventListener('click', () => {
+    openModal();
+
+  })
+})
+function openModal() {
+  document.getElementById("productModal").style.display = "block";
+}
+
+function closeModal() {
+  document.getElementById("productModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  productImage.src = `/images/image-product-${n}.jpg`;
+}
+
+function showSlides(n) {
+  let i;
+  const slides = document.getElementsByClassName("mySlides");
+
+  if (n > slides.length) { slideIndex = 1 };
+  if (n < 1) { slideIndex = slides.length };
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
